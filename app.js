1c8218964f365b5ffb292018ea8ea4e921869fb4
itKyryl <it.kyryl@gmail.com>
2022-08-24 00:50:07 +0200

    Update Reacct history

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
+const connect = require('connect-history-api-fallback');
 
 dotenv.config();
 
 const app = express();
+app.use(connect());
 
 if(process.env.REACT_APP_MODE === "production") {
     app.use(express.static('build'))

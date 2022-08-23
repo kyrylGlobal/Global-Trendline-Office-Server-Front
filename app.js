727b8802abbd6c51a1f0c1a0465995075df618db
itKyryl <it.kyryl@gmail.com>
2022-08-24 00:42:24 +0200

    Fix

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 if(process.env.REACT_APP_MODE === "production") {
     app.use(express.static('build'))
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, "index.html"))
     })
 }
